9b4e847cdb62cc46cbbd3d8df6b90b7842982d4e
BrunoCaribe <44852946+BrunoCaribe@users.noreply.github.com>
2018-12-03 18:25:30 -0300

    Create ClerigoAction.java

---
+++ Xadrez/src/com/xadrez/actions/ClerigoAction.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xadrez.actions;
+
+import com.xadrez.core.Action;
+import com.xadrez.core.Peca;
+import com.xadrez.core.Xadrez;
+import com.xadrez.core.XadrezButton;
+import com.xadrez.estructure.Position;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+/**
+ *
+ * @author brcar
+ */
+public class ClerigoAction extends Action {
+
+    Peca me;
+    Peca p;
+    public ClerigoAction(Xadrez x,Peca p) {
+        super(x);
+        me=p;
+    }
+
+    @Override
+    public void executeAction(ActionEvent e) {
+        Position pos = me.getPosition();
+         ArrayList<Peca> pecas = xadrez.tabuleiro.GetPecasInRange(pos.x,pos.y,5);
+
+         XadrezButton button = (XadrezButton)e.getSource(); 
+          p =  xadrez.tabuleiro.GetPeca(button.coord_x, button.coord_y);
+
+         if(p!=null){
+              System.out.println("Peca pegada");
+              System.out.println(p.nome);
+
+              for(Peca pe:pecas)
+                {
+                    if(pe.getPosition().x == pe.getPosition().x && pe.getPosition().y == pe.getPosition().y){
+                       p.vida += 10;
+                       me.coolDown+=4;
+                       xadrez.acao=xadrez.ACAOPADRAO;
+                       System.out.println("Peca curada");
+                     break;
+                    }
+                }
+
+
+         }else{
+             System.out.println("Espaco vazio, selecione uma peca valida");
+         }
+
+    }
+}
+
