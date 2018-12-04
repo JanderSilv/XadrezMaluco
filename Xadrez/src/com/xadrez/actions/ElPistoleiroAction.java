f406286c9d2e057bed2772a2024ca5a44919f182
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2018-12-04 17:31:16 -0200

    Mudança de pasta do PistoleiroAction
    Co-Authored-By: jeguenu <jeguenu@users.noreply.github.com>

---
+++ Xadrez/src/com/xadrez/actions/ElPistoleiroAction.java
@@ -0,0 +1,38 @@
+package com.xadrez.actions;
+
+import com.xadrez.core.Action;
+import com.xadrez.core.Jogador;
+import com.xadrez.core.Peca;
+import com.xadrez.core.Xadrez;
+import com.xadrez.estructure.Position;
+import java.awt.event.ActionEvent;
+/**
+ *
+ * @author Jeguenu
+ */
+
+public class ElPistoleiroAction extends Action{
+    
+    Jogador jogador;
+    Peca peca;
+    
+    public ElPistoleiroAction(Xadrez x, Peca p) {      
+        super(x); 
+        peca = p;
+    }
+
+    @Override
+    public void executeAction(ActionEvent e) {
+       Position pos = peca.getPosition();
+       Peca esq = xadrez.tabuleiro.GetPeca(pos.x-1, pos.y);
+       Peca dir = xadrez.tabuleiro.GetPeca(pos.x+1, pos.y);
+       if(esq!=null && dir!=null){
+           esq.vida -= 20;
+           dir.vida -= 20;
+           peca.coolDown += 5;
+           xadrez.acao=xadrez.acaoPadrao;
+       }
+       
+    }
+    
+}
