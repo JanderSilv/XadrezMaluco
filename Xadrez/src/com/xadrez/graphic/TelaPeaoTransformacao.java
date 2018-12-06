4313067b586f3e7bbdb2d19f029659fded55f6d1
JanderSilv <jander.500@hotmail.com>
2018-12-06 03:17:46 -0300

    Desing da TelaPeao

--- Xadrez/src/com/xadrez/graphic/TelaPeaoTransformacao.java
+++ Xadrez/src/com/xadrez/graphic/TelaPeaoTransformacao.java
@@ -10,6 +10,7 @@ import com.xadrez.actions.TransformPeaoAction;
 import com.xadrez.core.Xadrez;
 import com.xadrez.estructure.Position;
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -62,15 +63,23 @@ public class TelaPeaoTransformacao extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        txt_titulo = new javax.swing.JLabel();
+        canvasBackgrounds1 = new com.xadrez.graphic.CanvasBackgrounds();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList_listaPecas = new javax.swing.JList<>();
+        txt_titulo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(244, 323));
+        setMinimumSize(new java.awt.Dimension(244, 323));
+        setPreferredSize(new java.awt.Dimension(244, 323));
 
-        txt_titulo.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txt_titulo.setText("SELECIONE UMA PEÇA");
+        canvasBackgrounds1.setImg(new ImageIcon("src\\com\\xadrez\\imagens\\cemiterio.jpg"));
+        canvasBackgrounds1.setMaximumSize(new java.awt.Dimension(244, 323));
+        canvasBackgrounds1.setMinimumSize(new java.awt.Dimension(244, 323));
+        canvasBackgrounds1.setPreferredSize(new java.awt.Dimension(244, 323));
 
+        jList_listaPecas.setBackground(new java.awt.Color(255, 255, 204));
+        jList_listaPecas.setFont(new java.awt.Font("Consolas", 0, 11)); // NOI18N
         jList_listaPecas.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
@@ -78,27 +87,45 @@ public class TelaPeaoTransformacao extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jList_listaPecas);
 
+        txt_titulo.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
+        txt_titulo.setText("SELECIONE UMA PEÇA");
+
+        javax.swing.GroupLayout canvasBackgrounds1Layout = new javax.swing.GroupLayout(canvasBackgrounds1);
+        canvasBackgrounds1.setLayout(canvasBackgrounds1Layout);
+        canvasBackgrounds1Layout.setHorizontalGroup(
+            canvasBackgrounds1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(canvasBackgrounds1Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(canvasBackgrounds1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(canvasBackgrounds1Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(canvasBackgrounds1Layout.createSequentialGroup()
+                        .addComponent(txt_titulo, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
+                        .addContainerGap())))
+        );
+        canvasBackgrounds1Layout.setVerticalGroup(
+            canvasBackgrounds1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, canvasBackgrounds1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(txt_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(45, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(26, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(txt_titulo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(canvasBackgrounds1, javax.swing.GroupLayout.PREFERRED_SIZE, 245, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(txt_titulo, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(31, 31, 31)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(canvasBackgrounds1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -107,6 +134,7 @@ public class TelaPeaoTransformacao extends javax.swing.JFrame {
   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private com.xadrez.graphic.CanvasBackgrounds canvasBackgrounds1;
     private javax.swing.JList<String> jList_listaPecas;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel txt_titulo;
